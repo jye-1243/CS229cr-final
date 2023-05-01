85447be87048ba12871b45210a5010bec07ecc10
Justin Ye <justinye@holygpu2c0710.rc.fas.harvard.edu>
2023-04-30 23:52:30 -0400

    Separate plotting

--- network_to_adj_fc.py
+++ network_to_adj_fc.py
@@ -214,7 +214,7 @@ DESCRIPTIONS = [
 ]
 PATH_END_LOTTO = "/replicate_1/level_0/main/model_ep0_it0.pth"
 
-
+'''FIEDLER '''
 # # THIS IS FOR FIEDLER
 
 # with open('fiedler_3.csv', 'w', newline='') as csvfile:
@@ -301,6 +301,8 @@ PATH_END_LOTTO = "/replicate_1/level_0/main/model_ep0_it0.pth"
 #                 fiedler = get_fiedler(del_A)
 #                 writer.writerow([DESCRIPTIONS[index], l, n, sparsity, fiedler])
 
+# _______________________________________________________________________________________________________
+'''SPECTRAL '''
 # with open('spectral_expansions_randomized_1.csv', 'w', newline='') as csvfile:
 #     writer = csv.writer(csvfile, delimiter=',')
 #     writer.writerow(['Model', 'Level', "Exp Number", 'Sparsity', 'Spectral Expansion', 'Performance', 'is_lotto'])
@@ -427,47 +429,3 @@ PATH_END_LOTTO = "/replicate_1/level_0/main/model_ep0_it0.pth"
 
 #             writer.writerow([DESCRIPTIONS[index], l, sparsity, np.mean(leverages), np.std(leverages)])
 
-
-'''
-CODE FOR PLOTTING: Sparsity vs. Spectral Expansion
-'''
-  
-x = []
-y = []
-x_rand = []
-y_rand = []
-  
-with open('spectral_expansions_200_50.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ',')
-      
-    for row in plots:
-        x.append(row[2])
-        y.append(row[3])
-
-x = [float(i) for i in x[1:]]
-y = [float(i) for i in y[1:]]
-
-with open('spectral_expansions_randomized_1.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter = ',')
-      
-    for row in plots:
-        x_rand.append(row[3])
-        y_rand.append(row[4])
-
-x_rand = np.array([float(i) for i in x_rand[1:]])
-x_rand = np.average(x_rand.reshape(-1, 5), axis=1)
-print(x_rand)
-
-y_rand = np.array([float(i) for i in y_rand[1:]])
-y_rand_std = np.std(y_rand.reshape(-1, 5), axis=1)
-y_rand = np.average(y_rand.reshape(-1, 5), axis=1)
-print(y_rand)
-print(y_rand_std)
-
-plt.scatter(x, y, c='orange', marker='x', label="lottery")
-plt.errorbar(x_rand, y_rand, yerr=(2*y_rand_std), linestyle=None, c='blue', marker = "o", label = "random submatrix")
-plt.xlabel('Sparsity')
-plt.ylabel('Spectral Expansion')
-plt.title('Sparsity vs. Spectral Expansion, mnist_lenet_200_50')
-plt.legend()
-plt.show()
---
+++ plotting.py
@@ -0,0 +1,51 @@
+import numpy as np
+import torch
+import math
+import csv
+import json
+
+from matplotlib import pyplot as plt
+
+'''
+CODE FOR PLOTTING: Sparsity vs. Spectral Expansion
+'''
+  
+x = []
+y = []
+x_rand = []
+y_rand = []
+  
+with open('spectral_expansions_200_50.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ',')
+      
+    for row in plots:
+        x.append(row[2])
+        y.append(row[3])
+
+x = [float(i) for i in x[1:]]
+y = [float(i) for i in y[1:]]
+
+with open('spectral_expansions_randomized_1.csv','r') as csvfile:
+    plots = csv.reader(csvfile, delimiter = ',')
+      
+    for row in plots:
+        x_rand.append(row[3])
+        y_rand.append(row[4])
+
+x_rand = np.array([float(i) for i in x_rand[1:]])
+x_rand = np.average(x_rand.reshape(-1, 5), axis=1)
+print(x_rand)
+
+y_rand = np.array([float(i) for i in y_rand[1:]])
+y_rand_std = np.std(y_rand.reshape(-1, 5), axis=1)
+y_rand = np.average(y_rand.reshape(-1, 5), axis=1)
+print(y_rand)
+print(y_rand_std)
+
+plt.scatter(x, y, c='orange', marker='x', label="lottery")
+plt.errorbar(x_rand, y_rand, yerr=(2*y_rand_std), linestyle=None, c='blue', marker = "o", label = "random submatrix")
+plt.xlabel('Sparsity')
+plt.ylabel('Spectral Expansion')
+plt.title('Sparsity vs. Spectral Expansion, mnist_lenet_200_50')
+plt.legend()
+plt.show()
